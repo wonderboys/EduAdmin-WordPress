ccdea12e21f8084cb80df8e66e619f367ff1a34d
Chris Gårdenberg <chga@multinet.se>
2017-01-19 10:23:07 +0100

    Aaaand phrasing, you have to echo out the phrase with sprintf

--- content/template/bookingTemplate/__checkEmail.php
+++ content/template/bookingTemplate/__checkEmail.php
@@ -16,9 +16,9 @@
 			break;
 	}
 	?>
-	<h3><?php sprintf(edu_e("Please enter your %s to continue."), $loginLabel); ?></h3>
+	<h3><?php echo sprintf(edu__("Please enter your %s to continue."), $loginLabel); ?></h3>
 	<label>
-		<div class="inputLabel"><?php $loginLabel; ?></div>
+		<div class="inputLabel"><?php echo $loginLabel; ?></div>
 		<div class="inputHolder">
 			<input type="email" name="eduadminloginEmail" required title="<?php echo esc_attr(sprintf(edu__("Please enter your %s here"), $loginLabel)); ?>" placeholder="<?php echo esc_attr($loginLabel); ?>" value="<?php echo @esc_attr($_REQUEST["eduadminloginEmail"]); ?>" />
 		</div>
