01c4e816973837d7ca61a54556d50ebe21ec221b
Chris Gårdenberg <chga@multinet.se>
2017-01-19 15:20:19 +0100

    Error supression

--- backend/edu.api.functions.php
+++ backend/edu.api.functions.php
@@ -10,7 +10,7 @@ if (!function_exists('getallheaders'))
        {
            if (substr($name, 0, 5) == 'HTTP_')
            {
-               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+               @$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
            }
        }
        return $headers;
