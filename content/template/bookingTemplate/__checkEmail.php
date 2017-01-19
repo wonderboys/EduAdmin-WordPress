30d20d92cb4485a542e4b70401fce10194e30776
Chris Gårdenberg <chga@multinet.se>
2017-01-19 10:31:34 +0100

    A civic reg number isn't an email, you dufus.

--- content/template/bookingTemplate/__checkEmail.php
+++ content/template/bookingTemplate/__checkEmail.php
@@ -20,7 +20,7 @@
 	<label>
 		<div class="inputLabel"><?php echo $loginLabel; ?></div>
 		<div class="inputHolder">
-			<input type="email" name="eduadminloginEmail" required title="<?php echo esc_attr(sprintf(edu__("Please enter your %s here"), $loginLabel)); ?>" placeholder="<?php echo esc_attr($loginLabel); ?>" value="<?php echo @esc_attr($_REQUEST["eduadminloginEmail"]); ?>" />
+			<input type="text" name="eduadminloginEmail" required title="<?php echo esc_attr(sprintf(edu__("Please enter your %s here"), $loginLabel)); ?>" placeholder="<?php echo esc_attr($loginLabel); ?>" value="<?php echo @esc_attr($_REQUEST["eduadminloginEmail"]); ?>" />
 		</div>
 	</label>
 	<input type="submit" class="bookingLoginButton" value="<?php echo esc_attr(edu__("Continue")); ?>" />
--- content/template/myPagesTemplate/loginPage.php
+++ content/template/myPagesTemplate/loginPage.php
@@ -23,7 +23,7 @@
 			<label>
 				<div class="loginLabel"><?php echo $loginLabel; ?></div>
 				<div class="loginInput">
-					<input type="email" name="eduadminloginEmail" required title="<?php echo esc_attr(edu__("Please enter your e-mail address here")); ?>" placeholder="<?php echo esc_attr($loginLabel); ?>" value="<?php echo @esc_attr($_REQUEST["eduadminloginEmail"]); ?>" />
+					<input type="text" name="eduadminloginEmail" required title="<?php echo esc_attr(edu__("Please enter your e-mail address here")); ?>" placeholder="<?php echo esc_attr($loginLabel); ?>" value="<?php echo @esc_attr($_REQUEST["eduadminloginEmail"]); ?>" />
 				</div>
 			</label>
 			<label>
