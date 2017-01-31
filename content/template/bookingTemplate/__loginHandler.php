3a8401869775bb290b11de089ea8301ecb662085
Chris Gårdenberg <chga@multinet.se>
2017-01-31 11:34:06 +0100

    Simplified loginfield-filter

--- content/template/bookingTemplate/__loginHandler.php
+++ content/template/bookingTemplate/__loginHandler.php
@@ -5,17 +5,10 @@ if(isset($_REQUEST['bookingLoginAction']) && !empty($_REQUEST['bookingLoginActio
 	{
 		$ft = new XFiltering();
 		$selectedLoginField = get_option('eduadmin-loginField', 'Email');
-		switch($selectedLoginField)
-		{
-			case "Email":
-				$f = new XFilter('Email', '=', trim($_REQUEST['eduadminloginEmail']));
-				break;
-			case "CivicRegistrationNumber":
-				$f = new XFilter('CivicRegistrationNumber', '=', trim($_REQUEST['eduadminloginEmail']));
-				break;
-		}
 
+		$f = new XFilter($selectedLoginField, '=', trim($_REQUEST['eduadminloginEmail']));
 		$ft->AddItem($f);
+
 		$f = new XFilter('Disabled', '=', false);
 		$ft->AddItem($f);
 
